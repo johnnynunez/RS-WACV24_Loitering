64e6ffdda4dad87b8d13daf69e8b2b9655a6459c
johnnynunez <johnnynuca14@gmail.com>
2023-10-30 11:01:42 +0100

    .

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="Anomaly_Detection",
+    version="0.0.1",
+    description="Identify Loitering Behavior with Trajectory Analysis",
+    author="Johnny Núñez Cano",
+    author_email="jnunezca11@alumnes.ub.edu",
+    url="https://github.com/johnnynunez/RS-WACV24_Loitering",
+    install_requires=["torch"],
+)
